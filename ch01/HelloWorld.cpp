54ade6070f5a85a77febba838a03cd242d00957f
SangDon Park <sdp14222@gmail.com>
2022-04-19 15:09:02 +0900

    Create HelloWorld.cpp

---
+++ ch01/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int num = 20;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
