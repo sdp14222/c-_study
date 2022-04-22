dae9cbd6cde2a73a7079efdb3745065711ac49c0
SangDon Park <sdp14222@gmail.com>
2022-04-22 17:29:16 +0900

    Create NameSp1.cpp

---
+++ ch01/namespace/NameSp1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace BestComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout << "BestCom이 정의한 함수" << std::endl;
+	}	
+} // namespace BestComImpl
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout << "ProgCom이 정의한 함수" << std::endl;
+	}	
+} // namespace ProgComImpl
+
+
+
+int main(int argc, char const *argv[])
+{
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	
+	return 0;
+}
